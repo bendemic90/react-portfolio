0490ba38686d6e73418151ff82b69a955e338ee7
Ben T <ben.trevethan@gmail.com>
2021-05-06 18:34:02 +0930

    darkmode css

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,6 @@
 export default function Footer() {
     return (
-        <footer id="footer" className="navbar navbar-light justify-content-center footer bg-light">
+        <footer id="footer" className="navbar justify-content-center footer">
             <span className="navbar-text">
                 <i>FOOTER</i>
             </span>
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,6 +1,6 @@
 export default function Nav() {
     return (
-        <header id="header" className="navbar navbar-light justify-content-center bg-light">
+        <header id="header" className="navbar justify-content-center header">
             <span className="navbar-text">
                 <i>HEADER</i>
             </span>
