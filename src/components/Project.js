ceb574d05c5c4b19a2dfe8ab7d130e2ee1a7f2b7
Ben T <ben.trevethan@gmail.com>
2021-05-11 17:55:38 +0930

    project bios updated

--- src/components/Project.js
+++ src/components/Project.js
@@ -4,9 +4,10 @@ import cms from "../assets/img/cms.png";
 import emp from "../assets/img/employee.png"
 
 function Project() {
+  const projectsStr = '{...}'
   return (
     <>
-      <h1>projects()</h1><hr/>
+      <h1>projects{projectsStr}</h1><hr/>
       <a
         href="https://github.com/bendemic90/group-project-2"
         rel="noopener noreferrer"
@@ -14,14 +15,17 @@ function Project() {
       >
         <img className="dcode" src={dcode} width="250px" alt="dcode" />
       </a>
-      <br />
+      <p>DCODE is a modern tool for developers, specifically full-stack developers. The content within DCODE includes many helpful function documentations. Our goal in this project was to provide an 'encyclopedia' of code that will help beginners and advanced developers alike!</p>
       <a
         href="https://github.com/bendemic90/content-management-system/"
         rel="noopener noreferrer"
         target="_blank"
       >
         <img className="dcode" src={cms} width="250px" alt="cms" />
-      </a><br/>
+      </a>
+      <p>
+        An employee management system handled in CLI with `inquirer` to traverse the MYSQL database (handled with Sequelize ORM).
+      </p>
       <a
         href="https://bendemic90.github.io/react-employee-directory/"
         rel="noopener noreferrer"
@@ -29,6 +33,9 @@ function Project() {
       >
         <img className="dcode" src={emp} width="250px" alt="cms" />
       </a>
+      <p>
+        A create-react-app which loads a table filled with 30 random employees from RandomUserAPI. Dynamic filtering, sorting, and entirely function based with hooks - as opposed to Classes.
+      </p>
     </>
   );
 }
